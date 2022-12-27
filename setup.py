4434364846dc3a5181a5032c7a5a1f72a1d612c9
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2022-12-27 20:57:48 +0530

    Added __init__.py__

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="reddit-crawler",
+    name="reddit_crawler",
     version="0.0.1",
     description="A scraper which will scrape out multimedia data from reddit.",
-    long_description="""""",
+    long_description="""This package will help you scrape data from reddit for any of your data science or data analytics project. This is a wrapper on the PRAW package customized for easy data extraction.""",
     author="Aneesh Aparajit G",
-    packages=["reddit-crawler"],
+    packages=["reddit_crawler"],
     install_requires=["praw", "pandas", "numpy", "requests"],
 )
